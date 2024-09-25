318e5dace861035be3496fa7af87b70b1ac79a02
SVishal0099 <vishalshelar0099@gmail.com>
2024-09-25 10:07:06 +0530

    Update dictionary_which_contains_a_num.py

--- dictionary_which_contains_a_num.py
+++ dictionary_which_contains_a_num.py
@@ -1,5 +1,5 @@
 dict={}
-n=int(input("How many numbers do you want to add in Dictionar:"))
+n=int(input("How many numbers do you want to add in Dictionary:"))
 for x in range(1,n+1):
  dict[x]=x*x
 print(dict)
