489fc1a45e0fbc0c30ff5ba05237247621c38895
SVishal0099 <vishalshelar0099@gmail.com>
2024-10-12 14:57:43 +0530

    Add files via upload

---
+++ overloading.py
@@ -0,0 +1,5 @@
+list_of_colors = ['Red', 'White', 'Black']
+colors = '-'.join(list_of_colors) # *
+print()
+print("All Colors: "+colors)
+print()
---
+++ use_subclass_and_calculate_area_volume.py
@@ -0,0 +1,29 @@
+class Shape:
+    pass
+class Square(Shape):
+    def __init__(self,l2):
+        self.l=l2
+    def SArea(self):
+        a=self.l * self.l
+        print("Area of Square:", a)
+    def SPerimeter(self):
+        p=4 * self.l
+        print("Perimeter of Square:",p)
+class Circle(Shape):
+    def __init__(self,r2):
+        self.r=r2
+    def CArea(self):
+        a=3.14 * self.r * self.r
+        print("Area of Circle:", a)
+    def SCircumference(self):
+        c=2 * 3.14 * self.r
+        print("Circumference of Circle:",c)
+
+l1=int(input("Enter Length of Square: "))
+obj=Square(l1)
+obj.SArea()
+obj.SPerimeter()
+r1=int(input("Enter Radius of Circle: "))
+obj=Circle(r1)
+obj.CArea()
+obj.SCircumference()
